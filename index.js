394714928a9d8a46f791eb36977f22587ecf160a
mahinur05 <mahi121.mr2@gmail.com>
2023-02-08 22:50:58 +0600

    card showing

--- index.js
+++ index.js
@@ -1,78 +1,77 @@
-// const puppeteer = require('puppeteer');
-
-// // async function main() {
-// //   const browser = await puppeteer.launch();
-// //   const page = await browser.newPage();
-// //   await page.goto('https://www.google.com');
-// //   await page.screenshot({path: 'google.png'});
-// //   await browser.close();
-// // }
-
-// async function scrapeProduct(url) {
-//     // lunch the browser
-//     const browser = await puppeteer.launch()
-//     const page = await browser.newPage()
-//     await page.goto(url)
-
-//     // const [el] = await page.$x('//*[@id="landingImage"]');
-//     // const src = await el.getProperty('src');
-//     // const imgUrl = await src.jsonValue()
-
-//     // const [el2] = await page.$x('//*[@id="productTitle"]');
-//     // const txt = await el2.getProperty('textContent');
-//     // const title = await txt.jsonValue()
-
-//     // const [el3] = await page.$x('//*[@id="availability"]/span');
-//     // const txt2 = await el3.getProperty('textContent');
-//     // const available = await txt2.jsonValue()
-//     // console.log({ imgUrl, title, available});//*[@id="landingImage"] 
+const express = require('express');
+const app = express();
 
 
-
-//     browser.close();
-// }
-
-// // scrapeProduct('https://www.amazon.com/2022-Apple-MacBook-Laptop-chip/dp/B0B3C7MJX3/ref=sr_1_1?keywords=apple+mackbook%5C&qid=1675681752&sr=8-1');
-
-const puppeteer = require('puppeteer');
-
-async function scrapeProducts(url) {
+console.log('hello world')
+let array = [];
+async function searchProduct(url) {
+  const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
   const products = await page.$$('.s-result-item');
-  products.length = 10;
   for (const product of products) {
     const titleElement = await product.$('.a-size-medium');
     const priceElement = await product.$('.a-price-whole');
+    const fractionElement = await product.$('.a-price-fraction');
     const nameElement = await product.$('.a-color-base');
     const imageElement = await product.$('.s-image');
 
-    if (titleElement) {
-      const title = await titleElement.getProperty('textContent');
-      const titleValue = await title.jsonValue();
-      console.log(`Title: ${titleValue}`);
-    }
-    if (nameElement) {
+    if (nameElement && priceElement && imageElement) {
       const name = await nameElement.getProperty('textContent');
       const nameValue = await name.jsonValue();
-      console.log(`Name: ${nameValue}`);
-    }
-    if (priceElement) {
       const price = await priceElement.getProperty('textContent');
       const priceValue = await price.jsonValue();
-      console.log(`Price: ${priceValue}`);
-    }
-    if (imageElement) {
+      const fraction = await fractionElement.getProperty('textContent');
+      const fractionValue = await fraction.jsonValue();
       const image = await imageElement.getProperty('src');
       const imageValue = await image.jsonValue();
-      console.log(`Image: ${imageValue}`);
-    }
-    console.log('---------------------');
-  }
+      array.push({ nameValue, priceValue, fractionValue, imageValue });
+      if (array.length === 10) {
+        break;
+      }
+    } else if (titleElement && priceElement && imageElement) {
+      const title = await titleElement.getProperty('textContent');
+      const titleValue = await title.jsonValue();
+      const price = await priceElement.getProperty('textContent');
+      const priceValue = await price.jsonValue();
+      const fraction = await fractionElement.getProperty('textContent');
+      const fractionValue = await fraction.jsonValue();
+      const image = await imageElement.getProperty('src');
+      const imageValue = await image.jsonValue();
+      array.push({ titleValue, priceValue, fractionValue, imageValue });
+      if (array.length === 10) {
+        break;
+      }
 
+    }
+  }
+  console.log(array);
   await browser.close();
 }
 
-scrapeProducts('https://www.amazon.com/s?k=car+toy&crid=1TK2REW711M02&sprefix=car+to%2Caps%2C630&ref=nb_sb_noss_2');
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+app.get('/', (req, res) => {
+  let html = '<html><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Search Page</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></head><body class="container d-flex justify-content-center align-items-center"><div class="d-flex justify-content-center align-items-center row">';
+  array.forEach((item) => {
+    html += `
+    <div class="card col-3" style="width: 18rem; height: 470px">
+        <img src="${item.imageValue}" class="card-img-top" style="height: 10rem;" alt="...">
+        <div class="card-body">
+            <h5 class="card-text">Product Name: ${item.nameValue}</h5>
+            <p class="card-text">Price: ${item.priceValue}${item.fractionValue}$</p>
+            <a href="#" class="btn btn-primary">Add to cart</a>
+        </div>
+    </div>
+    `;
+  });
+  html += '</div></body></html>';
+  res.send(html);
+});
+
+searchProduct('https://www.amazon.com/s?k=car+toy&sprefix=%2Caps%2C778&ref=nb_sb_ss_recent_5_0_recent');
